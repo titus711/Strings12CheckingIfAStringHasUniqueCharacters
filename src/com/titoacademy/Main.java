634623abf4b41b3d1308fc274bb021ff7369de47
titus nangi <titusnangi@gmail.com>
2021-01-31 03:07:59 +0200

    Initial commit

---
+++ src/com/titoacademy/Main.java
@@ -0,0 +1,43 @@
+package com.titoacademy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        String myString = "TITUS";
+        char [] myChar = myString.toCharArray();
+        Map<Character, Integer> myMap = new HashMap<>();
+
+        for(int i = 0; i < myChar.length;i++){
+            if (myMap.containsKey(myChar[i]) == false){
+                myMap.put(myChar[i],1);
+            } else{
+                myMap.put(myChar[i],myMap.get(myChar[i]) + 1);
+            }
+
+        }
+
+        Set<Map.Entry<Character,Integer>> myEntireEntrySet = myMap.entrySet();
+        for (Map.Entry<Character,Integer> mySingleEntrySet:myEntireEntrySet){
+            if (mySingleEntrySet.getValue() > 1){
+                System.out.println("Does not contain UNIQUE characters");
+                System.exit(0);
+            }
+
+
+
+
+
+        }
+
+        System.out.println("Contains UNIQUE characters");
+
+
+
+    }
+}
